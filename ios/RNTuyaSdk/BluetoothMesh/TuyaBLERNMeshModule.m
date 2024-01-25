a1553c0fd2f9e51f06ee8c351df4f71633fdef09
Lilia <lilu.rudenko@gmail.com>
2024-01-25 15:41:06 +0100

    86dqt93f0 / Discovered device - iOS

--- ios/RNTuyaSdk/BluetoothMesh/TuyaBLERNMeshModule.m
+++ ios/RNTuyaSdk/BluetoothMesh/TuyaBLERNMeshModule.m
@@ -18,8 +18,6 @@ static TuyaBLERNMeshModule * scannerInstance = nil;
 
 @interface TuyaBLERNMeshModule()<ThingSmartSIGMeshManagerDelegate>
 
-// @property (nonatomic, assign) BOOL isSuccess;
-// @property (nonatomic, strong) NSMutableArray<ThingSmartSIGMeshDiscoverDeviceInfo *> *dataSource;
 @property (nonatomic, strong) ThingSmartSIGMeshManager *manager;
 @property (nonatomic, strong) NSMutableArray<ThingSmartSIGMeshDiscoverDeviceInfo *> *dataSource;
 
@@ -31,6 +29,7 @@ RCT_EXPORT_MODULE(TuyaBLEMeshModule)
 
 RCT_EXPORT_METHOD(stopScan:(RCTPromiseResolveBlock)resolver rejecter:(RCTPromiseRejectBlock)rejecter) {
   NSLog(@"---------------STOP");
+
   [ThingSmartSIGMeshManager.sharedInstance stopActiveDevice];
   [ThingSmartSIGMeshManager.sharedInstance stopSerachDevice];
   ThingSmartSIGMeshManager.sharedInstance.delegate = nil;
@@ -44,19 +43,16 @@ RCT_EXPORT_METHOD(startScan:(NSDictionary *)params) {
   long long homeId = ((NSNumber *)params[kTuyaRNMeshModuleHomeId]).longLongValue;
   ThingSmartHome *home = [ThingSmartHome homeWithHomeId:homeId];
   ThingSmartBleMeshModel *sigMeshModel = home.sigMeshModel;
-  NSLog(@"---------------sigMeshModel: %@", sigMeshModel);
 
   if (sigMeshModel) {
     NSLog(@"---------------sigMeshModel presented");
     [self performSearch:sigMeshModel];
   } else {
-    NSLog(@"---------------sigMeshModel NONE");
-
-    [ThingSmartBleMesh createSIGMeshWithHomeId:home.homeId success:^(ThingSmartBleMeshModel *meshModel) {
-      NSLog(@"---------------successfully created: %@", meshModel);  
-      [self performSearch:sigMeshModel];
+    [ThingSmartBleMesh createSIGMeshWithHomeId:homeId success:^(ThingSmartBleMeshModel *meshModel) {
+      NSLog(@"---------------meshModel successfully created: %@", meshModel);
+      [self performSearch:meshModel];
     } failure:^(NSError *error) {
-      NSLog(@"create mesh error: %@", error);
+      NSLog(@"---------------create mesh error: %@", error);
     }];
   }
 }
@@ -71,19 +67,12 @@ RCT_EXPORT_METHOD(startScan:(NSDictionary *)params) {
 }
 
 - (void)sigMeshManager:(ThingSmartSIGMeshManager *)manager didScanedDevice:(ThingSmartSIGMeshDiscoverDeviceInfo *)device{
-  NSLog(@"---------------device: %@", [device yy_modelToJSONObject]);
-
   TuyaEventSender * eventSender = [TuyaEventSender allocWithZone: nil];
   [eventSender sendEvent2RN:tuyaEventSenderScanLEEvent body:[device yy_modelToJSONObject]];
-
-
-  [ThingSmartSIGMeshManager.sharedInstance stopActiveDevice];
-  [ThingSmartSIGMeshManager.sharedInstance stopSerachDevice];
-  ThingSmartSIGMeshManager.sharedInstance.delegate = nil;
 }
 
 - (void)sigMeshManager:(ThingSmartSIGMeshManager *)manager didActiveSubDevice:(ThingSmartSIGMeshDiscoverDeviceInfo *)device devId:(NSString *)devId error:(NSError *)error{
-  NSLog(@"---------------didActiveSubDevice: %@", device);
+  // NSLog(@"---------------didActiveSubDevice: %@", device);
   // if (!error) {
   //     [self.dataSource removeObject:device];
   //     [self.tableView reloadData];
