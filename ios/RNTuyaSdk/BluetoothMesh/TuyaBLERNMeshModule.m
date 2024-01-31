d2b4359a5ef4e1b97e75a79ae8464a00e10074ee
Lilia <lilu.rudenko@gmail.com>
2024-01-31 21:01:53 +0100

    Added activator

--- ios/RNTuyaSdk/BluetoothMesh/TuyaBLERNMeshModule.m
+++ ios/RNTuyaSdk/BluetoothMesh/TuyaBLERNMeshModule.m
@@ -10,6 +10,7 @@
 #import <ThingSmartDeviceKit/ThingSmartHome.h>
 #import <ThingSmartBLEMeshKit/ThingSmartBLEMeshKit.h>
 #import "TuyaEventSender.h"
+#import "TuyaRNUtils+Network.h"
 #import "YYModel.h"
 
 #define kTuyaRNMeshModuleHomeId @"homeId"
@@ -20,6 +21,8 @@ static TuyaBLERNMeshModule * scannerInstance = nil;
 
 @property (nonatomic, strong) ThingSmartSIGMeshManager *manager;
 @property (nonatomic, strong) NSMutableArray<ThingSmartSIGMeshDiscoverDeviceInfo *> *dataSource;
+@property(copy, nonatomic) RCTPromiseResolveBlock promiseResolveBlock;
+@property(copy, nonatomic) RCTPromiseRejectBlock promiseRejectBlock;
 
 @end
 
@@ -27,9 +30,14 @@ static TuyaBLERNMeshModule * scannerInstance = nil;
 
 RCT_EXPORT_MODULE(TuyaBLEMeshModule)
 
-RCT_EXPORT_METHOD(stopScan:(RCTPromiseResolveBlock)resolver rejecter:(RCTPromiseRejectBlock)rejecter) {
-  NSLog(@"---------------STOP");
+- (NSMutableArray *)dataSource{
+  if (!_dataSource) {
+      _dataSource = NSMutableArray.new;
+  }
+  return _dataSource;
+}
 
+RCT_EXPORT_METHOD(stopScan:(RCTPromiseResolveBlock)resolver rejecter:(RCTPromiseRejectBlock)rejecter) {
   [ThingSmartSIGMeshManager.sharedInstance stopActiveDevice];
   [ThingSmartSIGMeshManager.sharedInstance stopSerachDevice];
   ThingSmartSIGMeshManager.sharedInstance.delegate = nil;
@@ -45,11 +53,9 @@ RCT_EXPORT_METHOD(startScan:(NSDictionary *)params) {
   ThingSmartBleMeshModel *sigMeshModel = home.sigMeshModel;
 
   if (sigMeshModel) {
-    NSLog(@"---------------sigMeshModel presented");
     [self performSearch:sigMeshModel];
   } else {
     [ThingSmartBleMesh createSIGMeshWithHomeId:homeId success:^(ThingSmartBleMeshModel *meshModel) {
-      NSLog(@"---------------meshModel successfully created: %@", meshModel);
       [self performSearch:meshModel];
     } failure:^(NSError *error) {
       NSLog(@"---------------create mesh error: %@", error);
@@ -61,23 +67,40 @@ RCT_EXPORT_METHOD(startScan:(NSDictionary *)params) {
   self.manager = [ThingSmartBleMesh initSIGMeshManager:sigMeshModel ttl:8 nodeIds:nil];
   self.manager.delegate = self;
 
-  NSLog(@"---------------startSearch");
-
+  [self.dataSource removeAllObjects];
   [self.manager startSearch];
 }
 
 - (void)sigMeshManager:(ThingSmartSIGMeshManager *)manager didScanedDevice:(ThingSmartSIGMeshDiscoverDeviceInfo *)device{
+  [self.dataSource addObject:device];
+
   TuyaEventSender * eventSender = [TuyaEventSender allocWithZone: nil];
   [eventSender sendEvent2RN:tuyaEventSenderScanLEEvent body:[device yy_modelToJSONObject]];
 }
-
 - (void)sigMeshManager:(ThingSmartSIGMeshManager *)manager didActiveSubDevice:(ThingSmartSIGMeshDiscoverDeviceInfo *)device devId:(NSString *)devId error:(NSError *)error{
-  // NSLog(@"---------------didActiveSubDevice: %@", device);
-  // if (!error) {
-  //     [self.dataSource removeObject:device];
-  //     [self.tableView reloadData];
-  //     [SVProgressHUD showSuccessWithStatus:[NSString stringWithFormat:@"%@ %@" ,NSLocalizedString(@"Successfully Added", @"") ,device.mac]];
-  // }
+  TuyaEventSender * eventSender = [TuyaEventSender allocWithZone: nil];
+  [eventSender sendEvent2RN:tuyaEventSenderDeviceAction body:nil];
+
+  if (scannerInstance.promiseResolveBlock) {
+    scannerInstance.promiseResolveBlock([device yy_modelToJSONObject]);
+  }
+}
+
+- (void)sigMeshManager:(ThingSmartSIGMeshManager *)manager didFailToActiveDevice:(ThingSmartSIGMeshDiscoverDeviceInfo *)device error:(NSError *)error{
+  if (scannerInstance.promiseRejectBlock) {
+    [TuyaRNUtils rejecterWithError:error handler:scannerInstance.promiseRejectBlock];
+  }
+}
+
+RCT_EXPORT_METHOD(activateDevice:(NSDictionary *)params resolver:(RCTPromiseResolveBlock)resolver rejecter:(RCTPromiseRejectBlock)rejecter) {
+  if (scannerInstance == nil) {
+    scannerInstance = [TuyaBLERNMeshModule new];
+  }
+
+  scannerInstance.promiseResolveBlock = resolver;
+  scannerInstance.promiseRejectBlock = rejecter;
+
+  [self.manager startActive:self.dataSource];
 }
 
 @end
--- src/mesh.ts
+++ src/mesh.ts
@@ -6,6 +6,10 @@ type StartScanParams = {
   homeId: number;
 };
 
+type DeviceParams = {
+  device: any
+}
+
 export function startMeshScan(params: StartScanParams) {
   if (Platform.OS === 'ios') {
     return tuya.startScan(params);
@@ -15,5 +19,11 @@ export function startMeshScan(params: StartScanParams) {
 export function stopMeshScan() {
   if (Platform.OS === 'ios') {
     return tuya.stopScan();
+  }
+}
+
+export function activateDevice(params: DeviceParams) {
+  if (Platform.OS === 'ios') {
+    return tuya.activateDevice(params);
   }
 }
