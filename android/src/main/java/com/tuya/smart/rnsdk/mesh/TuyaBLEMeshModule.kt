f380dcf2afef9bbb6f346b65dc100200482fb137
Lilia <lilu.rudenko@gmail.com>
2024-02-28 23:40:56 +0100

    Android mesh activator

--- android/src/main/java/com/tuya/smart/rnsdk/mesh/TuyaBLEMeshModule.kt
+++ android/src/main/java/com/tuya/smart/rnsdk/mesh/TuyaBLEMeshModule.kt
@@ -2,13 +2,17 @@ package com.tuya.smart.rnsdk.mesh
 
 import android.util.Log
 import com.facebook.react.bridge.*
+import com.facebook.react.modules.core.DeviceEventManagerModule.RCTDeviceEventEmitter
+import com.thingclips.smart.android.blemesh.api.IThingBlueMeshActivatorListener
 import com.thingclips.smart.home.sdk.ThingHomeSdk
 import com.thingclips.smart.home.sdk.api.IThingHome
 import com.thingclips.smart.sdk.bean.BlueMeshBean
 import com.thingclips.smart.android.blemesh.builder.SearchBuilder
+import com.thingclips.smart.android.blemesh.builder.ThingSigMeshActivatorBuilder
 import com.thingclips.smart.android.blemesh.api.IThingBlueMeshSearch
 import com.thingclips.smart.android.blemesh.api.IThingBlueMeshSearchListener
 import com.thingclips.smart.android.blemesh.bean.SearchDeviceBean
+import com.thingclips.smart.sdk.bean.DeviceBean;
 import com.thingclips.smart.home.sdk.bean.HomeBean
 import com.thingclips.smart.home.sdk.callback.IThingResultCallback
 import com.thingclips.smart.sdk.bean.SigMeshBean
@@ -30,17 +34,16 @@ class TuyaBLEMeshModule(reactContext: ReactApplicationContext) :
 
   companion object {
     const val ON_SCAN_BEAN_EVENT = "ON_SCAN_BEAN_EVENT"
+    const val ON_DEVICE_ACTION = "ON_DEVICE_ACTION"
   }
 
   @ReactMethod
   fun stopScan() {
-    Log.i("MYLOGS", "--------------STOP SEARCH")
     mMeshSearch?.stopSearch()
   }
 
   @ReactMethod
   fun startScan(params: ReadableMap, promise: Promise) {
-    Log.i("MYLOGS", "--------------START SEARCH")
     val homeId = params.getDouble(HOMEID).toLong()
     val mThingHome: IThingHome = ThingHomeSdk.newHomeInstance(homeId);
     val homeBean: HomeBean = mThingHome.homeBean
@@ -77,18 +80,12 @@ class TuyaBLEMeshModule(reactContext: ReactApplicationContext) :
 
     val iThingBlueMeshSearchListener: IThingBlueMeshSearchListener = object : IThingBlueMeshSearchListener {
       override fun onSearched(deviceBean: SearchDeviceBean) {
-        Log.i("MYLOGS", "--------------deviceBean: $deviceBean")
         dataSource.add(deviceBean)
 
         mMeshSearch?.stopSearch()
 
-        Log.i("MYLOGS", "--------------dataSource: " + dataSource.size)
-
-        TuyaReactUtils.sendEvent(reactApplicationContext, ON_SCAN_BEAN_EVENT, TuyaReactUtils.parseToWritableMap(deviceBean))
-
-        // reactApplicationContext.getJSModule(DeviceEventManagerModule.RCTDeviceEventEmitter::class.java).emit(
-        //   ON_SCAN_BEAN_EVENT, TuyaReactUtils.parseToWritableMap(deviceBean)
-        // )
+        val device = mapOf("mac" to deviceBean.macAdress, "productId" to deviceBean.productId)
+        reactApplicationContext.getJSModule(RCTDeviceEventEmitter::class.java).emit(ON_SCAN_BEAN_EVENT, TuyaReactUtils.parseToWritableMap(device))
       }
 
       override fun onSearchFinish() {}
@@ -103,18 +100,33 @@ class TuyaBLEMeshModule(reactContext: ReactApplicationContext) :
       .build()
 
     mMeshSearch = ThingHomeSdk.getThingBlueMeshConfig().newThingBlueMeshSearch(searchBuilder)
-
-
-/// CHECK IF IT NEEDEF TO RESUME
-    // ThingHomeSdk.getThingSigMeshClient().startSearch()
     mMeshSearch?.startSearch()
  }
 
   @ReactMethod
-  fun activateDevice(params: ReadableMap, promise: Promise) {
-    //  val towns: Array<String> = location.toTypedArray<String>()
-    // dataSource.toTypedArray
-    Log.i("MYLOGS", "--------------dataSource: $dataSource")
+  fun activateDevice(promise: Promise) {
+    val iThingBlueMeshActivatorListener: IThingBlueMeshActivatorListener = object : IThingBlueMeshActivatorListener {
+      override fun onSuccess(mac: String, deviceBean: DeviceBean) {
+        reactApplicationContext.getJSModule(RCTDeviceEventEmitter::class.java).emit(ON_DEVICE_ACTION, null)
+
+        promise.resolve(deviceBean.devId)
+      }
+
+      override fun onError(mac: String, errorCode: String, errorMsg: String) {
+        Log.d("MYLOGS", "--------------activator error:" + errorMsg);
+      }
+
+      override fun onFinish() { }
+    }
+
+    val tuyaSigMeshActivatorBuilder: ThingSigMeshActivatorBuilder = ThingSigMeshActivatorBuilder()
+      .setSearchDeviceBeans(dataSource)
+      .setSigMeshBean(ThingHomeSdk.getSigMeshInstance().sigMeshList[0])
+      .setTimeOut(300)
+      .setThingBlueMeshActivatorListener(iThingBlueMeshActivatorListener);
+
+    val iThingBlueMeshActivator = ThingHomeSdk.getThingBlueMeshConfig().newSigActivator(tuyaSigMeshActivatorBuilder);
+    iThingBlueMeshActivator.startActivator();
   }
 
   @ReactMethod
